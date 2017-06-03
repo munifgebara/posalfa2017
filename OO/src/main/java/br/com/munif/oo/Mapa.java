b28477a548de3213ecd9e43302aaff7b7cfc6864
Admin <Admin@Dell-Prof>
2017-06-03 13:23:58 -0300

    anda maria

---
+++ OO/src/main/java/br/com/munif/oo/Mapa.java
@@ -0,0 +1,51 @@
+package br.com.munif.oo;
+
+public class Mapa {
+    
+    
+    private static Mapa instancia=new Mapa();
+    
+    public static Mapa getInstancia(){
+        return instancia;
+    }
+
+    private Lugar lugarInicialJogador;
+    private Lugar lugarInicialNPC;
+
+    private Mapa() {
+        criaLugares();
+    }
+
+    private void criaLugares() {
+        Lugar sala = new Lugar("sala");
+        Lugar copa = new Lugar("copa");
+        Lugar corredor = new Lugar("corredor");
+        Lugar cozinha = new Lugar("cozina");
+        Lugar quarto = new Lugar("quarto");
+        Lugar banheiro = new Lugar("banheiro");
+        sala.setSul(copa);
+        copa.setNorte(sala);
+        copa.setSul(cozinha);
+        cozinha.setNorte(copa);
+        sala.setOeste(corredor);
+        copa.setOeste(corredor);
+        cozinha.setOeste(corredor);
+        corredor.setLeste(copa);
+        corredor.setNorte(quarto);
+        quarto.setSul(corredor);
+        corredor.setSul(banheiro);
+        banheiro.setNorte(corredor);
+        lugarInicialJogador = sala;
+        lugarInicialNPC = cozinha;
+    }
+
+    public Lugar getLugarInicialJogador() {
+        return lugarInicialJogador;
+    }
+
+    public Lugar getLugarInicialNPC() {
+        return lugarInicialNPC;
+    }
+    
+
+}
--- OO/src/main/java/br/com/munif/oo/Programa.java
+++ OO/src/main/java/br/com/munif/oo/Programa.java
@@ -1,20 +1,58 @@
 package br.com.munif.oo;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 public class Programa {
     
-    
     public static void main(String ... args){
-        Lugar casa=new Lugar("casa");
-        Personagem p1=new Jogador("Maria",casa);
-        Personagem p2=new NPC(casa);
+        Scanner scanner=new Scanner(System.in);
+        Mapa mapa=Mapa.getInstancia();
+
+        Personagem jogador=new Jogador("Maria",mapa.getLugarInicialJogador());
+        Personagem npc=new NPC(mapa.getLugarInicialNPC());
         
+        while (true){
+            Lugar lc=jogador.getLugar();
+            System.out.println(jogador.getNome()+"!");
+            System.out.println("Voce está no(a):"+
+                    lc.getDescricao());
+            
+            if (lc.getLeste()!=null){
+                System.out.println("1) Para leste "+lc.getLeste().getDescricao());
+            }
+            if (lc.getOeste()!=null){
+                System.out.println("2) Para Oeste "+lc.getOeste().getDescricao());
+            }
+            if (lc.getNorte()!=null){
+                System.out.println("3) Para Norte "+lc.getNorte().getDescricao());
+            }
+            if (lc.getSul()!=null){
+                System.out.println("4) Para Sul "+lc.getSul().getDescricao());
+            }
+            
+            int opcao=scanner.nextInt();
+            
+            if (opcao==1 && lc.getLeste()!=null){
+                jogador.setLugar(lc.getLeste());
+            }
+            else if (opcao==2 && lc.getOeste()!=null){
+                jogador.setLugar(lc.getOeste());
+            }
+            else if (opcao==3 && lc.getNorte()!=null){
+                jogador.setLugar(lc.getNorte());
+            }
+            else if (opcao==4 && lc.getSul()!=null){
+                jogador.setLugar(lc.getSul());
+            }
+            else{
+                System.out.println("Opcao inválida");
+            }
+            System.out.println("------------------");
+            
+            
+        }
         
     }
-    
-    
     
     public static void mainVelho(String []args){
         try {
